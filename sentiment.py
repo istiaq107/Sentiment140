ff889509aec1a5db882df2c75e96bf01c658cc68
istiaq107 <istiaq107@gmail.com>
2018-11-29 17:36:37 -0500

    sentiment updated.

--- sentiment.py
+++ sentiment.py
@@ -11,13 +11,20 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 with open("data.csv", "r") as file:
     data = np.array(list(csv.reader(file)))
-    
+print "Data acquired."
+
 features_train, features_test, labels_train, labels_test = model_selection.train_test_split(data[:, 5], data[:, 0], test_size=0.1, random_state=42)
+print "Features and labels split into training and testing sets."
+pdb.set_trace()
+
+features_train, features_test, labels_train, labels_test = features_train[:32000], features_test[:32000], labels_train[:32000], labels_test[:32000]
+print "Testing and training sets reduced in size."
 
 le = preprocessing.LabelEncoder()
 le.fit(labels_train)
 labels_train = le.transform(labels_train)
 labels_test = le.transform(labels_test)
+pdb.set_trace()
 
 vectorizer = TfidfVectorizer(encoding="ISO-8859-1")
 features_train_transformed = vectorizer.fit_transform(features_train).toarray()
