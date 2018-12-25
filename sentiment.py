566da5c924e9e687227623d55d0badfe4f04ed36
istiaq107 <istiaq107@gmail.com>
2018-12-24 19:01:08 -0500

    was able to reach 78% max accuracy with NB, moving onto different classifier.

--- sentiment.py
+++ sentiment.py
@@ -4,24 +4,21 @@ import re
 import numpy as np
 import pandas as pd
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import ComplementNB
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import matplotlib.pyplot as plt
 
-def drop_features(features,data):
-    data.drop(features,inplace=True,axis=1)
-
-def process_tweet(tweet):
+def preprocess_tweets(tweet):
     return " ".join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])", " ",tweet.lower()).split())
 
 train_data = pd.read_csv("train.csv")
-train_data = train_data[:3000]
+train_data = train_data[:7000]
 print "data acquired."
-train_data['SentimentText'] = train_data['SentimentText'].apply(process_tweet)
-drop_features(['ItemID'],train_data)
-print "data cleaned."
+train_data['SentimentText'] = train_data['SentimentText'].apply(preprocess_tweets)
+train_data.drop('ItemID', inplace=True, axis=1)
+print "data preprocessing complete."
 
 features_train, features_test, labels_train, labels_test = train_test_split(train_data['SentimentText'], train_data['Sentiment'], test_size=0.2, random_state=42)
 print "features and labels split into training and testing sets."
@@ -31,11 +28,16 @@ Vectorizer = TfidfVectorizer(stop_words='english', encoding="ISO-8859-1",subline
 features_train_tfidf = Vectorizer.fit_transform(features_train)
 features_test_tfidf = Vectorizer.transform(features_test)
 print "features vectorized"
-pdb.set_trace()
+# pdb.set_trace()
 
-gnb = GaussianNB()
+# plt.scatter(features_train_tfidf.toarray(), labels_train, color='red')
+
+gnb = ComplementNB(alpha=1.0)
 gnb.fit(features_train_tfidf, labels_train)
 print "classifier training complete."
-predicted = gnb.predict(features_test_tfidf)
+predicted = gnb.predict(features_test_tfidf.toarray())
+
+# plt.plot(features_train_tfidf, predicted, color='blue')
+# plt.show()
 
 print "Accuracy Score: ", accuracy_score(predicted, labels_test)
