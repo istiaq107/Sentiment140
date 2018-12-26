950cdfdfe3fe58846415eb65bf793e36d792f16e
istiaq107 <istiaq107@gmail.com>
2018-12-25 20:48:30 -0500

    classifier changed to RandomForestClassifier, moving onto training with full dataset on vm

--- sentiment.py
+++ sentiment.py
@@ -4,8 +4,8 @@ import re
 import numpy as np
 import pandas as pd
 
-from sklearn.naive_bayes import ComplementNB
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.pyplot as plt
@@ -13,31 +13,24 @@ import matplotlib.pyplot as plt
 def preprocess_tweets(tweet):
     return " ".join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])", " ",tweet.lower()).split())
 
-train_data = pd.read_csv("train.csv")
-train_data = train_data[:7000]
+train_data = pd.read_csv("data.csv")
+train_data = train_data[:15000]
 print "data acquired."
-train_data['SentimentText'] = train_data['SentimentText'].apply(preprocess_tweets)
-train_data.drop('ItemID', inplace=True, axis=1)
+train_data['tweet'] = train_data['tweet'].apply(preprocess_tweets)
+train_data.drop('id', inplace=True, axis=1)
 print "data preprocessing complete."
 
-features_train, features_test, labels_train, labels_test = train_test_split(train_data['SentimentText'], train_data['Sentiment'], test_size=0.2, random_state=42)
+features_train, features_test, labels_train, labels_test = train_test_split(train_data['tweet'], train_data['label'], test_size=0.2, random_state=3)
 print "features and labels split into training and testing sets."
-# pdb.set_trace()
-Vectorizer = TfidfVectorizer(stop_words='english', encoding="ISO-8859-1",sublinear_tf=True)
+Vectorizer = TfidfVectorizer(stop_words='english', encoding="ISO-8859-1", sublinear_tf=True)
 
 features_train_tfidf = Vectorizer.fit_transform(features_train)
 features_test_tfidf = Vectorizer.transform(features_test)
 print "features vectorized"
-# pdb.set_trace()
 
-# plt.scatter(features_train_tfidf.toarray(), labels_train, color='red')
-
-gnb = ComplementNB(alpha=1.0)
-gnb.fit(features_train_tfidf, labels_train)
+clf = RandomForestClassifier()
+clf.fit(features_train_tfidf, labels_train)
 print "classifier training complete."
-predicted = gnb.predict(features_test_tfidf.toarray())
-
-# plt.plot(features_train_tfidf, predicted, color='blue')
-# plt.show()
+predicted = clf.predict(features_test_tfidf)
 
 print "Accuracy Score: ", accuracy_score(predicted, labels_test)
